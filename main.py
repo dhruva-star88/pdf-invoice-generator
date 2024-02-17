5263c486cab9cc322e59cf3a58b7f420702cbfb8
dhruva-star88 <dhruvatej6565@gmail.com>
2024-02-17 23:09:34 +0530

    created pdfs for each invoice file and added invoice number

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 glob function is used to search for files that match a specific file pattern or name 
 """
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 
 filepaths = glob.glob("invoices/*.xlsx")
@@ -10,4 +12,16 @@ print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.set_auto_page_break(auto=False, margin=0)
+    pdf.add_page()
+    # stem extracts the main filename without extension
+    filename = Path(filepath).stem
+    # Extracts only the first item of list
+    invoice_nmbr = filename.split("-")[0]
+    """
+    We can get invoice_number and file name by using slice method
+    """
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.cell(w=0, h=12, txt=f"Invoice nr.{invoice_nmbr}", align="L", ln=1)
+    pdf.output(f"PDFs/{filename}.pdf")
